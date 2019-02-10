7c509cedd34885578ff63ea34b9701734409da4d
pcarderdev <31299967+pcarderdev@users.noreply.github.com>
2019-02-09 21:04:22 -0800

    Create Staging.js
    Created staging.js to handle displaying/hiding divs

---
+++ js/Staging.js
@@ -0,0 +1,23 @@
+function beginMathPractice() {
+    var x = document.getElementById("StartPage");
+    var y = document.getElementById("MathPractice");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
+
+function beginWordPractice() {
+    var x = document.getElementById("StartPage");
+    var y = document.getElementById("WordPractice");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
