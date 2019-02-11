588f571ed1839df3e2439e40d699ea1d502626ae
pcarderdev <31299967+pcarderdev@users.noreply.github.com>
2019-02-10 22:11:22 -0800

    Added grading for math practice
    Added grading and checking user input for the math practice section.

--- js/MathPractice.js
+++ js/MathPractice.js
@@ -12,7 +12,6 @@ function createIntList() {
     intArr[4] = getRandomLargeNum();
     intArr[5] = getRandomLargeNum();
 
-    console.log(intArr.toString())
     document.getElementById("IntList").innerHTML = intArr.toString();
     
 }
@@ -25,6 +24,35 @@ function retRandomInt() {
 function gradeAnswer() {
     // compares user input against the randomInt from function above
     // scoring criteria: exact -> 10 pts, +/- 5 -> 7 pts
+
+    var userWork = eval(document.getElementById("userInput").value);
+    var answer = document.getElementById("RandNumber").innerHTML;
+
+    if (userWork == answer) {
+        document.getElementById("Results").innerHTML = "CORRECT";
+    } else {
+        document.getElementById("Results").innerHTML = "INCORRECT";
+    }
+
+    getScore(userWork, answer);
+}
+
+function getScore(userAnswer, rightAnswer) {
+    var score;
+    var absDiff = Math.abs(userAnswer - rightAnswer);
+
+
+    if (absDiff >= 10) {
+        score = 0;
+    } else if (absDiff >= 2) {
+        score = 5;
+    } else if (absDiff >= 1) {
+        score = 7;
+    } else if (absDiff == 0) {
+        score = 10;
+    }
+    document.getElementById("Score").innerHTML = ("Your score for this round is:" + score);
+    return score;
 }
 
 function getRandomLargeNum() {
